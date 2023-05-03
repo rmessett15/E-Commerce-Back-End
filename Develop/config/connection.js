f3c61f2d5bee87f47027f7af27ef29d6b4fd5c67
rmessett15 <rmessett15@gmail.com>
2023-05-03 18:54:02 -0500

    added sequelize, mysql2, dotenv dpendencies, created .env file, added table structure to category model, added route functionality to category get and post routes with Dillmann

--- Develop/config/connection.js
+++ Develop/config/connection.js
@@ -1,12 +1,15 @@
-require('dotenv').config();
+require("dotenv").config();
 
-const Sequelize = require('sequelize');
+const Sequelize = require("sequelize");
+
+console.log(process.env.DB_NAME, process.env.DB_USER, process.env.DB_PW);
+console.log(process.cwd());
 
 const sequelize = process.env.JAWSDB_URL
   ? new Sequelize(process.env.JAWSDB_URL)
   : new Sequelize(process.env.DB_NAME, process.env.DB_USER, process.env.DB_PW, {
-      host: 'localhost',
-      dialect: 'mysql',
+      host: "localhost",
+      dialect: "mysql",
       dialectOptions: {
         decimalNumbers: true,
       },
